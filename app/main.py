825f390191e973b0d09fe52bbdaf5fa929f5325c
QingYuAnWayne <zhangxuwei@buaa.edu.cn>
2020-10-06 19:48:11 +0800

    1

--- app/main.py
+++ app/main.py
@@ -5,31 +5,31 @@ dirlist = []
 files = os.listdir('.')
 for f in files:
     file = f
-token = ""
-for line in file:
-    i = 0
-    while i < len(line):
-        token = ""
-        if line[i].isspace():
-            i += 1
-            continue
-        if line[i].isalpha():
-            try:
-                while line[i].isalnum():
+    token = ""
+    for line in file:
+        i = 0
+        while i < len(line):
+            token = ""
+            if line[i].isspace():
+                i += 1
+                continue
+            if line[i].isalpha():
+                try:
+                    while line[i].isalnum():
+                        token += line[i]
+                        i += 1
+                except IndexError:
+                    pass
+                symbol = Function.reserver(token)
+                if symbol == 'Unknown':
+                    print("Ident("+token+")")
+                else:
+                    print(symbol)
+            elif line[i].isdigit():
+                while line[i].isdigit():
                     token += line[i]
                     i += 1
-            except IndexError:
-                pass
-            symbol = Function.reserver(token)
-            if symbol == 'Unknown':
-                print("Ident("+token+")")
-            else:
-                print(symbol)
-        elif line[i].isdigit():
-            while line[i].isdigit():
-                token += line[i]
-                i += 1
-            print("Int("+token+")")
+                print("Int("+token+")")
 
 
 
